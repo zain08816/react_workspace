70b3c958c5b54ad9864f1fdbde1fa7b03632b8d8
Zain Ali <zain08816@gmail.com>
2020-01-27 11:35:06 -0500

    add some testing stuff

---
+++ test-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Create Profile
+        </p>
+        <a>
+          Skills
+        </a>
+        <button>
+          Create Profile
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
