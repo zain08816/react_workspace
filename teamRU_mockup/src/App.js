461f70c378d9c68344da4f8a2934c1829e93df79
Zain Ali <zain08816@gmail.com>
2020-02-01 12:33:40 -0500

    fix name

--- teamRU_mockup/src/App.js
+++ teamRU_mockup/src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
+          teamRU
+        </p>
+        <p>
           Create Profile
         </p>
         <a>
